d68d5f9bf64ab5eb34a075d3176cb74bd7ca9267
LMNTZZ <bervoetskevin@gmail.com>
2017-03-14 09:51:18 +0100

    Committing to project
    Additional changes
    
     On branch openinghours

--- src/AppBundle/Controller/Admin/OpeningsHoursController.php
+++ src/AppBundle/Controller/Admin/OpeningsHoursController.php
@@ -124,5 +124,4 @@ class OpeningsHoursController extends Controller
         return $result;
 
     }
-
 }
--- src/AppBundle/Repository/HoursRepository.php
+++ src/AppBundle/Repository/HoursRepository.php
@@ -13,7 +13,17 @@ use Doctrine\ORM\EntityRepository;
 
 class HoursRepository extends EntityRepository
 {
+    public function showOpeningHours() {
 
+        $dql =
 
+        $entityManager = $this->getDoctrine()->getManager();
+        $openingHoursRepository = $entityManager->getRepository('openingHours');
+        $openingHours = $openingHoursRepository->findAll();
+
+        foreach ($openingHours as $openingHour) {
+            echo sprintf("%s\n", $openingHour->getOpeningHours());
+        }
+    }
 
 }
